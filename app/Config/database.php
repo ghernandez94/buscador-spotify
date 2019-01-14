3f51e157589f98d9c186427ebe9824134a318b4f
German <ge.hernandezf@alumnos.duoc.cl>
2019-01-14 09:05:43 -0300

    db fix

---
+++ app/Config/database.php
@@ -0,0 +1,15 @@
+<?php
+class DATABASE_CONFIG {
+    public $search = array(
+        'datasource' => 'SearchSource'
+    );
+
+    public $album = array(
+        'datasource' => 'AlbumSource'
+    );
+
+    public $artist = array(
+        'datasource' => 'ArtistSource'
+    );
+}
+?>
